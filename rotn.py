4a1ac6c5047f62fc27438c5f5bf44424612b2971
Corrado Ignoti <corrado@corradoignoti.it>
2022-01-01 22:02:39 +0100

    Bug fix

--- rotn.py
+++ rotn.py
@@ -1,18 +1,23 @@
 class rotn:
     
+    # international alphabet
+    #alphabet = "abcdefghijklmnopqrstuvxyz" 
+    
+    #using only italian letters.
+    alphabet = "abcdefghilmnopqrstuvz" 
+
     # encrypt a plain text using rotn
     # number define the rot type (numnber=13 => rot13)
     def encrypt(self, plain_text, number):
         plain_text = plain_text.lower()
-        text = "abcdefghilmnopqrstuvz"
         final = ""
         finalencrypted = ''
-        rotation = text[number:] + text[:number]
+        rotation = self.alphabet[number:] + self.alphabet[:number]
 
         
         for i in range(len(plain_text)):
             if plain_text[i].isalpha():
-                final = rotation[text.index(plain_text[i])]
+                final = rotation[self.alphabet.index(plain_text[i])]
                 finalencrypted += final
             else:
                 finalencrypted += plain_text[i]
@@ -23,16 +28,13 @@ class rotn:
     # number define the rot type (number=13 => rot13)
     def decrypt(self, encrypted_text, number):
         encrypted_text = encrypted_text.lower()
-        #number = int(input('input rot: '))
-        #text = "abcdefghijklmnopqrstuvwxyz"
-        text = "abcdefghilmnopqrstuvz"
         final = ""
         finaldecrypted = ''
-        rotation = text[number:] + text[:number]
+        rotation = self.alphabet[number:] + self.alphabet[:number]
 
         for i in range(len(encrypted_text)):
             if encrypted_text[i].isalpha():
-                final = text[rotation.index(encrypted_text[i])]
+                final = self.alphabet[rotation.index(encrypted_text[i])]
                 finaldecrypted += final
             else:
                 finaldecrypted += encrypted_text[i]
