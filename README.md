6b5429f5af023443c41a471b4a05891fa79887d1
corrado.ignoti <corrado.ignoti@MacBook-Pro-mio.fritz.box>
2022-01-01 21:51:44 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# rotn
+A small class to cipher/decipher text using rot n algorithm.
+
+It's for educational use only. Use stronger cipher algorithm in real enviroments.
---
+++ rotn.py
@@ -0,0 +1,43 @@
+class rotn:
+    # encrypt a plain text using rotn
+    # number define the rot type (numnber=13 => rot13)
+
+    def __init__(self):
+        pass
+
+    def encrypt(self, plain_text, number):
+        plain_text = plain_text.lower()
+        text = "abcdefghilmnopqrstuvz"
+        final = ""
+        finalencrypted = ''
+        rotation = text[number:] + text[:number]
+
+        
+        for i in range(len(plain_text)):
+            if plain_text[i].isalpha():
+                final = rotation[text.index(plain_text[i])]
+                finalencrypted += final
+            else:
+                finalencrypted += plain_text[i]
+        
+        return finalencrypted
+
+    # decrypt a ciphred text using rotn
+    # number define the rot type (number=13 => rot13)
+    def decrypt(self, encrypted_text, number):
+        encrypted_text = encrypted_text.lower()
+        #number = int(input('input rot: '))
+        #text = "abcdefghijklmnopqrstuvwxyz"
+        text = "abcdefghilmnopqrstuvz"
+        final = ""
+        finaldecrypted = ''
+        rotation = text[number:] + text[:number]
+
+        for i in range(len(encrypted_text)):
+            if encrypted_text[i].isalpha():
+                final = text[rotation.index(encrypted_text[i])]
+                finaldecrypted += final
+            else:
+                finaldecrypted += encrypted_text[i]
+        
+        return finaldecrypted
